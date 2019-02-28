210a2433814f70652718c9ad96f8a4d193d62c58
Pedro Henrique <pedro.henrique@aurea.com>
2019-02-28 12:55:30 -0300

    first commit

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldBean.java
@@ -0,0 +1,18 @@
+package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
+
+public class HelloWorldBean {
+
+    private final String message;
+
+    public HelloWorldBean(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override public String toString() {
+        return String.format("HelloWorldBean [message=%s]", message);
+    }
+}
